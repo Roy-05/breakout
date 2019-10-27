245ed65f7b4a778cd47a3099a2868c1b0502c2bf
Saket Roy <roy05@live.missouristate.edu>
2019-10-27 17:33:48 +0530

    Add brick elements to canvas

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ var canvas = document.getElementById("game-canvas"),
     paddleWidth = 75,
     paddleX = (canvas.width-paddleWidth)/2,
     rightPressed = false,
-    leftPressed = false;
+    leftPressed = false,
+    brickRowCount = 3,
+    brickColumnCount = 5,
+    brickWidth = 75,
+    brickHeight = 20,
+    brickPadding = 10,
+    brickOffsetTop = 30,
+    brickOffsetLeft = 30;
+
+var bricks = [];
+for(let c=0; c<brickColumnCount; c++){
+    bricks[c] = [];
+    for(let r=0; r<brickRowCount; r++){
+        bricks[c][r] = {x:0, y:0};
+    }
+}
 
 function drawBall() {
     ctx.beginPath();
@@ -25,6 +40,22 @@ function drawPaddle() {
     ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
+}
+
+function drawBricks() {
+    for(let c=0; c<brickColumnCount; c++){
+        for(let r=0; r<brickRowCount; r++){
+            var brickX = c*(brickWidth+brickPadding)+brickOffsetLeft,
+                brickY = r*(brickHeight+brickPadding)+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath()
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillstyle = "yellow";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
 }
 
 function keydownHandler(e) {
@@ -44,17 +75,26 @@ function keyupHandler(e) {
         leftPressed = false;
     }
 }
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
+    drawBricks();
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if( y + dy < ballRadius) {
         dy = -dy;
     }
+    else if( y + dy > canvas.height - ballRadius){
+        alert("Game Over!");
+        document.location.reload();
+        clearInterval(interval);
+    }
+
+
     if(rightPressed){
         paddleX +=5;
         if(paddleX + paddleWidth > canvas.width){
@@ -75,4 +115,5 @@ function draw() {
 
 document.addEventListener("keydown", keydownHandler, false);
 document.addEventListener("keyup", keyupHandler, false);
-setInterval(draw, 10);
+
+var interval = setInterval(draw, 10);
