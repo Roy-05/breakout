2173d7e85313aa32bb26cbc929a22f6c811af968
Saket Roy <roy05@live.missouristate.edu>
2019-10-27 17:57:51 +0530

    Add basic collision detection mechanism

--- index.js
+++ index.js
@@ -22,7 +22,21 @@ var bricks = [];
 for(let c=0; c<brickColumnCount; c++){
     bricks[c] = [];
     for(let r=0; r<brickRowCount; r++){
-        bricks[c][r] = {x:0, y:0};
+        bricks[c][r] = {x:0, y:0, status: 1};
+    }
+}
+
+function collisionDetection(){
+    for(let c=0; c<brickColumnCount; c++){
+        for(let r=0; r<brickRowCount; r++){
+           var b = bricks[c][r];
+           if(b.status == 1){
+                if(x > b.x && x < (b.x+brickWidth) && y > b.y && y < (b.y+brickHeight)){
+                    dy=-dy;
+                    b.status = 0;
+                }   
+            }
+        }
     }
 }
 
@@ -45,15 +59,17 @@ function drawPaddle() {
 function drawBricks() {
     for(let c=0; c<brickColumnCount; c++){
         for(let r=0; r<brickRowCount; r++){
-            var brickX = c*(brickWidth+brickPadding)+brickOffsetLeft,
-                brickY = r*(brickHeight+brickPadding)+brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath()
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillstyle = "yellow";
-            ctx.fill();
-            ctx.closePath();
+            if(bricks[c][r].status == 1){
+                var brickX = c*(brickWidth+brickPadding)+brickOffsetLeft,
+                    brickY = r*(brickHeight+brickPadding)+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath()
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillstyle = "yellow";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
@@ -81,6 +97,7 @@ function draw() {
     drawBall();
     drawPaddle();
     drawBricks();
+    collisionDetection();
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -89,11 +106,15 @@ function draw() {
         dy = -dy;
     }
     else if( y + dy > canvas.height - ballRadius){
-        alert("Game Over!");
-        document.location.reload();
-        clearInterval(interval);
+        if( x > paddleX && x < (paddleX + paddleWidth)){
+            dy=-dy;
+        }
+        else{
+            alert("Game Over!");
+            document.location.reload();
+            clearInterval(interval);
+        }
     }
-
 
     if(rightPressed){
         paddleX +=5;
